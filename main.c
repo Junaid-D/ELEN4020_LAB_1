ea2e8d3279e279aefddef565e0c27235165bebe8
Junaid-D <1094837@students.wits.ac.za>
2019-02-14 18:44:34 +0000

    added flattening to 1d tensor code

--- main.c
+++ main.c
@@ -1,17 +1,47 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct Tensor
 {
 	int noDims;
 	int* elemPerDim;
-	int* flatMat;
+	int* flattened;
 	int totalEntries;
 
-}tensor;
+}Tensor;
+
+void initFlat(Tensor* t)
+{
+	t->flattened= (int*)malloc(t->totalEntries*sizeof(int));
+	for(int i=0; i<t->totalEntries; i++)
+		t->flattened[i]=0;
+
+}
+
+int totalElements(int noDims, int* elemPerDim)
+{
+	int ans=1;
+	for(int i=0; i<noDims; i++)
+	{
+		ans*=elemPerDim[i];
+	}
+	return ans;
+
+}
+
 
 
 int main()
 {
+	int elemPer_0[] = {10,10};
 
+	Tensor t;
+
+	t.noDims=2;
+	t.elemPerDim= elemPer_0;
+	t.totalEntries=totalElements(t.noDims,t.elemPerDim);
+	initFlat(&t);
+
+	free(t.flattened);
 	return 0;
 }
